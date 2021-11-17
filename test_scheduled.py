20794d2443aef2fe3d4da3368e0680c97be1ad95
Tamás Börcsök <tborcsok@gmail.com>
2021-11-17 22:26:17 +0100

    change logging format

--- test_scheduled.py
+++ test_scheduled.py
@@ -36,7 +36,7 @@ if __name__=='__main__':
     started = datetime.utcnow()
 
     logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s %(message)s',
+                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                         handlers=[logging.FileHandler(root/'logs'/f'schedule_{started.date().isoformat()}.log'),
                                   logging.StreamHandler()])
 
